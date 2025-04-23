4d698b86c5d892095a7691fa66ff42e4b9978927
yogesh5454 <yogesh111sapkota@gmail.com>
2025-04-23 08:24:41 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+import Nav from './navbar/nav'
+function App() {
+  return <>
+  <Nav/>
+  </>
+}
+
+export default App
---
+++ src/navbar/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Nav = () => {
+  <div>
+    <div>
+        <img src="https://pickbazar-react-rest.vercel.app/_next/image?url=https%3A%2F%2Fpickbazarlaravel.s3.ap-southeast-1.amazonaws.com%2F2295%2FLogo-new.png&w=3840&q=75" alt="" />
+    </div>
+    <div className='dropdown'>
+        <button>Clothing</button>
+    </div>
+  </div>
+};
+
+export default Nav;

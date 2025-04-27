9f1c659652d783f12950b44e074c10effd7365fb
yogesh5454 <yogesh111sapkota@gmail.com>
2025-04-27 11:39:23 +0545

    update

--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -4,7 +4,7 @@ import { BsFillCartCheckFill } from "react-icons/bs";
 const Cart = () => {
   return (
   
-        <div className=' text-white bg-green-700 fixed p-3 top-4 right-4 mt-100 rounded-sm'>
+        <button className=' text-white bg-green-700 fixed p-3 top-4 right-4 mt-100 rounded-sm'>
             <div className='flex gap-2 items-center'>
         <BsFillCartCheckFill />
         <p>0 Item</p>
@@ -12,7 +12,7 @@ const Cart = () => {
         <div>
             <p className='p-1 bg-white text-green-700 rounded-sm'>$0.00</p>
         </div>
-        </div>
+        </button>
     
 
   )
